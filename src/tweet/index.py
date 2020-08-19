a538e378752b3f788013e161320122faa9e2d192
yuta-natsume <natsume.yuta@classmethod.jp>
2020-08-19 20:57:24 +0900

    use bare layer

--- src/tweet/index.py
+++ src/tweet/index.py
@@ -1,6 +1,9 @@
 from main import app, main
+import trend_app_protect.start
+from trend_app_protect.api.aws_lambda import protect_handler
 
 
+@protect_handler
 @app.decorator.auto_logging
 def handler(event, context):
     main(event)
